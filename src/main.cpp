71e2ae629d6bdf3db1818c81976e57d72044b6c5
Jose Alberto Barrantes <jabarrantes@apamedesign.com>
2021-04-27 19:27:20 -0600

    First part of the file parser added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int int main(int argc, const char** argv) {
+std::vector<string> parseLine(std::string Line) {
+    
+}
 
 
+void FileManager(std::string FileName) {
+    std::fstream f(FileName);
+    std::string text;
+    std::vector<string> parsedPuntos;
+    while (getline(f, text)) {
+        std::cout << text;
+        parseLine(text);
+        parsedPuntos.push_back();
+    }
+}
+
+
+
+int FileChose() {
+   return 0; 
+}
+
+
+int main(int argc, const char** argv) {
+    if (argc > 1)
+    {
+        std::string FILENAME = argv[1];
+        std::cout << "File input \n";
+        std::cout << FILENAME << "\n";
+        FileManager(FILENAME);
+    }
+    else {
+        FileChose();
+    }
+    
     return 0;
-
 }
