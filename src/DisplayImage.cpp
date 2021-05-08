4a4520b6ee9f660fa79856dec8d0eb3302cd546b
Alex Graham <alecu0825@gmail.com>
2021-05-08 14:19:29 -0600

    mejor escalamiento para la imagen

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -16,30 +16,36 @@ int main (void){
     char hull_window[] = "Drawing: Convex Hull";
     //vector <float> puntos{0, 0, 10, 0, 10, 10, 0, 10};
     vector <float> puntos{-40, 70, 80, -50, -80, -65};
-    //190 es el valor maximo de la pantalla, escalamos el punto maximo al 190.
-    float escalar = 190 / *max_element(puntos.begin(), puntos.end());
-    //cuando se tiene el valor de escalar, se multiplica ese escalar con todos los puntos del vector
-    std::transform(puntos.begin(),puntos.end(),puntos.begin(),
-      std::bind(std::multiplies<float>(), std::placeholders::_1,escalar));
+    //normalizamos los puntos y luego los escalamos al tamano de ventana
+    float max = *max_element(puntos.begin(), puntos.end());
+    float min = *min_element(puntos.begin(), puntos.end());
+    for (int i = 0; i < puntos.size(); i++) {
+        puntos[i] = (puntos[i] - min) / (max - min); //normalizamos el vector
+        puntos[i] = puntos[i] * 280 + 60;    //lo ajustamos al rango de 60 a 340
+    }
+    //std::transform(puntos.begin(),puntos.end(),puntos.begin(),
+    //  std::bind(std::multiplies<float>(), std::placeholders::_1,escalar));
     Mat hull_image = Mat::zeros( w, w, CV_8UC3 );
     putText(hull_image,"OpenCV",Point(w/2,w/2),FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2);
 
+    //creamos lineas
     for (int i=0; i < puntos.size();i=i+2){
       if (i != puntos.size()-2) {
-        MyLine(hull_image, Point(puntos[i] + w/2, w/2 - puntos[i+1]), 
-        Point(puntos[i+2] + w/2 , w/2 - puntos[i+3]));
+        MyLine(hull_image, Point(puntos[i], w - puntos[i+1]), 
+        Point(puntos[i+2] , w - puntos[i+3]));
       }
       else
-        MyLine(hull_image, Point(puntos[i] + w/2, w/2 - puntos[i+1]), 
-        Point(puntos[0]+w/2, w/2 - puntos[1]));
+        MyLine(hull_image, Point(puntos[i], w - puntos[i+1]), 
+        Point(puntos[0], w - puntos[1]));
     }
-      
+    //construimos la imagen  
     imshow( hull_window, hull_image );
     moveWindow( hull_window, 0, 200 );
     waitKey( 0 );
     return(0);
 }
 
+//funcion para lineas
 void MyLine( Mat img, Point start, Point end )
 {
   int thickness = 2;
